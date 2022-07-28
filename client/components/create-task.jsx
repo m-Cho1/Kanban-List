8a2881c37da9ec896c424c96441e1d61ac1c041d
m-Cho1 <mturtle051@gmail.com>
2022-07-28 01:23:35 +0000

    created button in home for modal pop up with form

--- client/components/create-task.jsx
+++ client/components/create-task.jsx
@@ -1,9 +1,49 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
-export default function CreateTask(props) {
-  return (
+export default class CreateTask extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false
+    };
+    this.handleShow = this.handleShow.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+  }
+
+  handleShow() {
+    this.setState({ isOpen: true });
+  }
+
+  handleClose() {
+    this.setState({ isOpen: false });
+  }
+
+  render() {
+    const { handleShow, handleClose } = this;
+    return (
     <>
-    <h1>This is the Home page</h1>
+    <Button variant="primary" onClick={handleShow}>
+        Create a Task
+      </Button>
+
+      <Modal show={this.state.isOpen} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>This will be form</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
-  );
+    );
+  }
+
 }
--- client/components/hello-world.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function HelloWorld(props) {
-  return (
-    <>
-    <h1>This is the Home page</h1>
-    </>
-  );
-}
